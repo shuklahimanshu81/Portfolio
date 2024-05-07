181c6556aacc5987b5f79761e92d11f2d253dd33
Himanshu Shukla <himanshu2361999@gmail.com>
2024-05-07 21:07:32 +0530

    fix : removed pro icon packages

--- components/utils/icon.util.jsx
+++ components/utils/icon.util.jsx
@@ -4,15 +4,10 @@ import { useEffect, useState } from 'react'
 // Font Awesome packages
 const { library, config } = require('@fortawesome/fontawesome-svg-core')
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { fat } from '@fortawesome/pro-thin-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+
 
 // Load icons into
-library.add(fat, fal, fas, fad, far, fab)
+
 
 /**
  * Icon factory utility.
@@ -35,7 +30,7 @@ export default function Icon({ icon }) {
 
 	const [ iconType, iconKey ] = icon
 
-	const [ stateIconKey, setIconKey ] = useState('circle-notch')
+	const [ stateIconKey, setIconKey ] = useState('')
 
 	useEffect( () => setIconKey( iconKey ), [ iconKey ] )
 
